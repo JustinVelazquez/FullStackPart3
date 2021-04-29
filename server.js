96ab7e55f198ac7bf032f8b1e9b47093381f8378
Justin V <justinv4994@gmail.com>
2021-04-28 23:41:25 -0400

    heroku deployment testing

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('build'));
 app.use(morgan('tiny'));
 
 morgan.token('body', (req) => JSON.stringify(req.body));
